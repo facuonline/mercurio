06442492da49dc34b3174337bd257a70e5ad5260
facuonline <37064182+facuonline@users.noreply.github.com>
2019-05-04 08:47:28 +0200

    Update gid.class.php

--- app/src/php/classes/mroutils/gid.class.php
+++ app/src/php/classes/mroutils/gid.class.php
@@ -11,22 +11,20 @@
  * _Not collision free_ but extremely unlikely to happen.
  * 
  * Not fixed length, with enough (a lot of years) time GIDs will overflow 64 bits
- * 
- * @var int $GID Stores the generated GID
  */
 
 namespace MroUtils;
 class GID {
-    private static $GID;
 
 	/**
 	 * Create new int GID
-	 *
-	 * Timestamp base with custom epoch
-	 * 1 digit server ip based discriminator
-	 * 1 digit connection port based discriminator
-	 * 1 digit client ip based discriminator
-	 * 1 digit random entropic discriminator */
+	 * 
+	 * Timestamp base with custom epoch.\  
+	 * 1 digit server ip based discriminator.\
+	 * 1 digit connection port based discriminator.\
+	 * 1 digit client ip based discriminator.\
+	 * 1 digit random entropic discriminator.
+	 * @return int */
 	public static function new(){
 		// Custom epoch from the january 1st 2019
 		$time = time() - 1546300800;
@@ -34,31 +32,25 @@ class GID {
 		$port = substr($_SERVER['REMOTE_PORT'], -1);
 		$client = substr(base_convert($_SERVER['REMOTE_ADDR'], 10, 10), -1);
 		$entropy = random_int(0, 9);
-
-		self::$GID = $time.$server.$port.$client.$entropy;
-		return (int) self::$GID;
+		return $time.$server.$port.$client.$entropy;
 	}
 
 	/**
-	 * Encrypts and decrypts a GID to prevent session hijacking and csrf
+	 * Encrypts and decrypts a GID to prevent CSRF
 	 *
 	 * Encrypted values are session wise decryptable 
 	 *
 	 * @param int|string $GID GID to encrypt or decrypt
 	 * @return int|string */
 	public static function enc($GID = false){
-		if ($GID) { 
-			self::$GID = $GID;
-		} else {
-			self::new();
-		}
+		if (!$GID) $GID = self::new();
 		/* aes-192-ctr is chosen because it allows for faster system performance in encryption and this function is supposed to provide an obfuscation layer rather than actually cryptosafe values, that's also the reason for no random IV */
-		if (ctype_digit(self::$GID)) {
+		if (ctype_digit($GID)) {
 			$iv = substr(md5(getenv('APP_KEY')), -16);
-			return openssl_encrypt(self::$GID, 'aes-192-ctr', session_id(), 0, $iv);
+			return openssl_encrypt($GID, 'aes-192-ctr', session_id(), 0, $iv);
 		} else {
 			$iv = substr(md5(getenv('APP_KEY')), -16);
-			return (int) openssl_decrypt(self::$GID, 'aes-192-ctr', session_id(), 0, $iv);
+			return openssl_decrypt($GID, 'aes-192-ctr', session_id(), 0, $iv);
 		}
 	}
 }
