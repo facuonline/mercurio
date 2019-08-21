e665791ece0f0cadcd64636e2024ab3c1b502deb
facuonline <37064182+facuonline@users.noreply.github.com>
2019-08-21 07:29:50 +0200

    Refactored ID test to be more strict

--- tests/utils/idTest.php
+++ tests/utils/idTest.php
@@ -1,7 +1,9 @@
 <?php
 namespace Mercurio\Test;
 
-$_SERVER['REMOTE_PORT'] = 0120;
+// We fake this because this is what ID will find in real world environments
+$_SERVER['REMOTE_PORT'] = '1234';
+$_SERVER['REMOTE_ADDR'] = '123.45.678.9';
 
 class IdTest extends \PHPUnit\Framework\TestCase {
 
@@ -12,41 +14,55 @@ class IdTest extends \PHPUnit\Framework\TestCase {
     }
 
     /**
-     * Utils\ID::new() must be able to retrieve at least 200 different numbers in a second
+     * Utils\ID::new() must be able to retrieve at least 1000 different numbers in a second
      */
     public function testNewReturnsDifferentNumbers() {
+        // We fake this because this is what ID will find in real world environments
+        $_SERVER['REMOTE_PORT'] = '1234';
+        $_SERVER['REMOTE_ADDR'] = '123.45.678.9';
         $start = time();
         
         $i = 0;
-        while ($i < 99) {
+        while ($i < 499) {
             $id1 = \Mercurio\Utils\ID::new();
-            // delay for 0.01 second
-            usleep(10000);
+            /* Delay for 0.006 second not precisely to simulate system latency 
+            but the fastest human latency actually */
+            usleep(6000);
             $id2 = \Mercurio\Utils\ID::new();
 
-            $this->assertNotEquals($id1, $id2, "ID generation test nº$i");
+            $this->assertNotEquals($id1, $id2, "ID generation test nº$i:");
             $i++;
         }
 
-        if ($i == 99) {
+        if ($i == 499) {
             $end = time();
             $result = $end - $start;
 
-            $this->assertLessThanOrEqual(2, $result);
+            // We check against 4 to take in consideration the hardcoded delay that takes 3 seconds
+            $this->assertLessThanOrEqual(4, $result);
         }
     }
     
-    public function testEncReturnsEncryptedOnEmptyCall() {
-        $id = \Mercurio\Utils\ID::enc();
+    public function testEncryptReturnsEncryptedOnEmptyCall() {
+        $id = \Mercurio\Utils\ID::encrypt();
         
         $this->assertIsString($id);
     }
 
-    public function testEncReturnsStringOnIntInput() {
+    public function testEncryptReturnsStringOnIntInput() {
         $id = '1234567890000';
-        $id = \Mercurio\Utils\ID::enc($id);
+        $id = \Mercurio\Utils\ID::encrypt($id);
 
         $this->assertIsString($id);
+    }
+
+    public function testDecryptReturnsDecryptedInteger() {
+        $id = '1234567890000';
+        $id = \Mercurio\Utils\ID::encrypt($id);
+        $id = \Mercurio\Utils\ID::decrypt($id);
+
+        $this->assertIsInt($id);
+        $this->assertEquals('1234567890000', $id);
     }
 
 }
