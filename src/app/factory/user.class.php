88bbdedcc3087f213466ecf0bb35059ecd656038
facuonline <37064182+facuonline@users.noreply.github.com>
2019-09-13 18:36:07 +0200

    Added app factories

---
+++ src/app/factory/user.class.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Mercurio\App\Factory;
+
+/**
+ * User factory
+ * @package Mercurio
+ * @subpackage App factories
+ */
+class User {
+
+    public function __construct() {
+        
+    }
+
+}
