15d8eec8d0ad93e63424018475c2bf81dd416a2e
facuonline <37064182+facuonline@users.noreply.github.com>
2019-09-17 08:08:54 +0200

    Improved Form factory

--- src/utils/form.class.php
+++ src/utils/form.class.php
@@ -6,7 +6,7 @@ namespace Mercurio\Utils;
  * Form creator \
  * Non static Util
  * 
- * Extended from nette/forms \
+ * Extended from `nette/forms`
  * @see https://doc.nette.org/en/3.0/forms
  * @package Mercurio
  * @subpackage Utilitary classes
@@ -14,25 +14,26 @@ namespace Mercurio\Utils;
 class Form {
 
     /**
-     * Instance of \Nette\Forms\Form
+     * Instance of `\Nette\Forms\Form`
      */
     public $form;
 
     /**
      * Start new form object
      * @param string $key Unique personalized key to avoid spambots from bypassing the filter
+     * @param string $csrf Message to be displayed by CSRF protector
      */
-    public function __construct(string $key = '') {
+    public function __construct(string $key = '', string $csrf = 'Security token has expired, please submit the form again.') {
         $this->form = new \Nette\Forms\Form;
 
         $this->form->onSuccess[] = $this->form->isSuccess();
+        $this->form->addProtection($csrf);
         $this->addSpamProtection($key);
     }
 
     /**
      * Add hidden honeypot fields to protect against automated SPAM
      * @param string $key Your own personalized key to avoid spambots from bypassing the filter
-     * @return object \Nette\Forms\Form
      */
     public function addSpamProtection(string $key = '') {
         $this->form->addHidden('url_website_pot'.$key)
@@ -45,8 +46,29 @@ class Form {
     }
 
     /**
+     * Build a basic login form ready to match with `\Mercurio\App\User` login methods
+     * @param string $credential_label Label for 'credential' form field
+     * @param string $credential_placeholder Placeholder for 'credential' input
+     * @param string $password_label Label for 'password' form field
+     * @param string $password_placeholder Placeholder for 'password' input
+     * @param string $submit Value of submit button
+     * @return \Nette\Form\Form 
+     */
+    public function login(string $credential_label = 'Username or password:', string $credential_placeholder = 'john@example.com', $password_label = 'Password:', $password_placeholder = 'My password', $submit = 'login') {
+        $this->form->addText('credential', $credential_label)
+            ->addHtmlAttribute('placeholder', $credential_placeholder)
+            ->setRequired(true);
+        $this->form->addPassword('password', $password_label)
+            ->addHtmlAttribute('placeholder', $password_placeholder)
+            ->setRequired(true);
+        $this->form->addSubmit($submit);
+        
+        return $this->getForm();
+    }
+
+    /**
      * Obtain Form object instance
-     * @return object \Nette\Forms\Form
+     * @return \Nette\Forms\Form
      */
     public function getForm() {
         return $this->form;
